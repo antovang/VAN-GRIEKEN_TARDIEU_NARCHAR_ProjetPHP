6907e789e70622c9ea99c34250662adde57cb25d
antovang <vangriea@antonios-macbook-air.home>
2020-04-04 21:48:16 +0200

    Nettoyage avant rendu

--- model/DAO.test.firtN.php
+++
@@ -1,9 +0,0 @@
-<?php require_once('DAO.class.php');
-
-    $config = parse_ini_file('../config/config.ini');
-
-    $dao = new DAO($config['database_path']);
-
-    $res = $dao->getFirstN(5);
-
-    var_dump($res);
--- model/DAO.test.getArtCat.php
+++
@@ -1,7 +0,0 @@
-<?php require_once('DAO.class.php');
-
-    $config = parse_ini_file('../config/config.ini');
-
-    $dao = new DAO($config['database_path']);
-
-    var_dump($dao->getCatArtN(1,12));
